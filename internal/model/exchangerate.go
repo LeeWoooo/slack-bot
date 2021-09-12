c496b0b42832a620bdc4494c758fb1fb9e298c3b
LeeWoooo <leecoding2285@gmail.com>
2021-09-13 00:09:24 +0900

    👔Update: transferKWR

--- internal/model/exchangerate.go
+++ internal/model/exchangerate.go
@@ -7,6 +7,5 @@ type ExchangeRate struct {
 	KRW         string // slack message를 보내는 시점의 환율(원화)
 	DtD         string // 전일 대비 환율 증가 감소 DATA
 	TransferKWR string // 송금보낼 때의 가격
-	Preference  string // Preference 우대 환율 KWR
 	ImageURL    string // 한달 환율 변동 그래프 URL
 }
