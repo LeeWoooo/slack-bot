a83bedaca4a8584816a1e12bab2849e5c1537a9b
LeeWoooo <leecoding2285@gmail.com>
2021-09-15 01:38:03 +0900

    test

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -21,7 +21,7 @@ func main() {
 	// create cron
 	c := cron.New()
 	// 매년 매월 월요일~금요일 아침 9시 15분
-	c.AddFunc("every 1s", func() {
+	c.AddFunc("@every 1s", func() {
 		// err := bot.SendTransfer()
 		// if err != nil {
 		// 	log.Fatal(err)
